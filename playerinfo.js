f5414181c77d7a9fab6ba8990857c428cedd7a77
Bele-Boss <109167486+Bele-Boss@users.noreply.github.com>
2023-10-10 11:49:52 +0530

    Update playerinfo.js

--- playerinfo.js
+++ playerinfo.js
@@ -1,66 +1,47 @@
-const API_KEY = 'AIzaSyCfxg14LyZ1hrs18WHUuGOnSaJ_IJEtDQc';
-const SHEET_ID = '1Bcl1EVN-7mXUP7M1FL9TBB5v4O4AFxGTVB6PwqOn9ss';
-
 function fetchPlayerInfo() {
-    // Your logic to fetch and display player info
-    console.log("fetchPlayerInfo is called");
-    // Example: Display a message in 'playerInfo' div
-    document.getElementById('playerInfo').innerText = "Fetching Player Info...";
-}
+    const name = document.getElementById('name').value;
+    const number = document.getElementById('number').value;
 
-
-    // Validate input
+    // Validate inputs
     if (!name || !number) {
-        alert('Please enter name and number');
+        alert('Please enter both name and number.');
         return;
     }
 
-    // Fetch player info from "snooker plus*" sheet
-    fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/snooker plus*!A2:Z1000?key=${API_KEY}`)
-        .then(response => response.json())
-        .then(data => {
-            // Find the player and display info
-            const playerRow = data.values.find(row => row[2] === name && row[4] === number);
-            if (playerRow) {
-                displayPlayerInfo(playerRow);
-                fetchFramesInfo(name);
-            } else {
-                alert('Player not found');
-            }
-        })
-        .catch(error => console.error('Error fetching data:', error));
-}
+    // Fetch data from Google Sheets
+    // Note: You'll need to use your API key and sheet ID
+    const API_KEY = 'AIzaSyCfxg14LyZ1hrs18WHUuGOnSaJ_IJEtDQc';
+    const SHEET_ID = '1Bcl1EVN-7mXUP7M1FL9TBB5v4O4AFxGTVB6PwqOn9ss';
+    const SHEET_NAME = 'Snookerplus*';
 
-function displayPlayerInfo(playerRow) {
-    // Display player info in a card
-    const playerInfoDiv = document.getElementById('playerInfo');
-    playerInfoDiv.innerHTML = `
-        <h2>${playerRow[2]}</h2>
-        <p>Table Money: ${playerRow[?]}</p>
-        <p>Total Money: ${playerRow[?]}</p>
-        <p>Balance: ${playerRow[?]}</p>
-    `;
-}
+    // Initialize Google Sheets API client
+    gapi.load('client', () => {
+        gapi.client.init({
+            apiKey: API_KEY,
+            discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+        }).then(() => {
+            // Fetch player data from "Snookerplus*" sheet
+            gapi.client.sheets.spreadsheets.values.get({
+                spreadsheetId: SHEET_ID,
+                range: SHEET_NAME,
+            }).then((response) => {
+                const values = response.result.values;
+                // Find player data and display it
+                // Note: You'll need to add logic here to find the relevant player data
+                // and then update the DOM to display it.
 
-function fetchFramesInfo(playerName) {
-    // Fetch frames info from "Frames" sheet
-    fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/Frames!A2:Z1000?key=${API_KEY}`)
-        .then(response => response.json())
-        .then(data => {
-            // Filter frames for the player and display
-            const playerFrames = data.values.filter(row => row[12] === playerName || row[13] === playerName || /*...other columns...*/ row[17] === playerName);
-            displayFramesInfo(playerFrames);
-        })
-        .catch(error => console.error('Error fetching data:', error));
-}
+                // Example: 
+                const player = values.find(row => row[2] === name && row[4] === number);
+                if (player) {
+                    document.getElementById('playerInfo').innerText = `Name: ${player[2]}, Total: ${player[5]}, Table Money: ${player[6]}`;
+                } else {
+                    document.getElementById('playerInfo').innerText = 'Player not found.';
+                }
+            }, (response) => {
+                console.error('Error fetching data:', response.result.error.message);
+            });
 
-function displayFramesInfo(playerFrames) {
-    // Display frames info in a list
-    const framesInfoDiv = document.getElementById('framesInfo');
-    framesInfoDiv.innerHTML = '<h2>Frames</h2>';
-    playerFrames.forEach(frame => {
-        framesInfoDiv.innerHTML += `
-            <p>Date: ${frame[2]}, Winner: ${frame[5]}, Duration: ${frame[3]} minutes</p>
-        `;
+            // Fetch and display frame data similarly...
+        });
     });
 }
