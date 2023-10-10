b887900b27256797b147548edb2ee61f16062c1f
Bele-Boss <109167486+Bele-Boss@users.noreply.github.com>
2023-10-10 11:56:07 +0530

    Create player_details.js

---
+++ player_details.js
@@ -0,0 +1,24 @@
+// Your API Key and Sheet ID
+const API_KEY = 'AIzaSyCfxg14LyZ1hrs18WHUuGOnSaJ_IJEtDQc';
+const SHEET_ID = '1Bcl1EVN-7mXUP7M1FL9TBB5v4O4AFxGTVB6PwqOn9ss';
+const PLAYER_SHEET_NAME = 'Snookerplus*';
+const FRAMES_SHEET_NAME = 'Frames';
+
+// Initialize Google Sheets API client
+function initClient() {
+    // Initialize API client and fetch data...
+}
+
+// Function to fetch and display player info
+function fetchPlayerInfo(playerName) {
+    // Fetch player info from "Snookerplus*" sheet and display it...
+}
+
+// Function to fetch and display frames info
+function fetchFramesInfo(playerName) {
+    // Fetch frames info from "Frames" sheet and display it...
+}
+
+// Example usage (replace 'John Doe' with the desired player name)
+fetchPlayerInfo('John Doe');
+fetchFramesInfo('John Doe');
